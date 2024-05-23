0c970eccf59de540d6f7a3a4b93c4b3a58cf094c
haptanova03@gmail.com <haptanova03@gmail.com>
2024-05-23 10:00:10 +0800

    1

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* function compareVersion(version1, version2) {
+const compareVersion = (version1, version2) => {
     const v1 = version1.split('.').map(Number);
     const v2 = version2.split('.').map(Number);
 
@@ -14,7 +14,7 @@
     }
 
     return 0;
-}; */
+};
 
 //export default compareVersion;
 
